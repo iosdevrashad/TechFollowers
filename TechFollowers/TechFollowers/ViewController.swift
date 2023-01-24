3e62cc0986ac4b60046eac3e1234ed9739fca171
iosdevrashad <rashad8787@me.com>
2023-01-24 10:28:01 -0600

    Initial State
    Created the project.

---
+++ TechFollowers/TechFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechFollowers
+//
+//  Created by Rashad Surratt on 1/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
